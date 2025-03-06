07c87d611bd5bf0757382dc72852b07633f96daa
peramalho <pedrorfig27@gmail.com>
2025-03-06 19:12:39 -0300

    feat: change font sizes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Button } from "./Button";
 function App() {
   return (
     <div className="flex flex-col min-h-screen items-center">
-      <div className="flex-1 flex flex-col w-[440px] py-16 items-center gap-8">
-        <p className="text-xl">Me fila</p>
-        <Button>Criar Fila</Button>
-        <Button>Entrar em Fila</Button>
+      <div className="flex-1 flex flex-col min-w-[440px] py-16 items-center gap-32">
+        <h1 className="text-[60px]">Me fila</h1>
+        <div className="flex flex-col items-center gap-8 w-full px-12">
+          <Button>Criar Fila</Button>
+          <Button>Entrar em Fila</Button>
+        </div>
       </div>
     </div>
   );
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,7 +1,7 @@
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className="bg-white text-black cursor-pointer p-1 rounded-md hover:bg-gray-200"
+      className="bg-white text-black cursor-pointer p-3 w-full sm:w-auto rounded-md hover:bg-gray-200 text-[40px]"
       {...props}
     />
   );
