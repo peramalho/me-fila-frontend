0151ed035ad06f686f64e9f46a201768bd1cc041
peramalho <pedrorfig27@gmail.com>
2025-03-06 19:24:15 -0300

    feat: add react-router

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
-import { Button } from "./Button";
+import { Button } from "../../Button";
 
-function App() {
+function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center">
       <div className="flex-1 flex flex-col min-w-[440px] py-16 items-center gap-32">
@@ -14,4 +14,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
