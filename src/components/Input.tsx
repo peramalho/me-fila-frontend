7c1914b8bbfd802ee57e0855aa522769b2c0cf71
peramalho <pedrorfig27@gmail.com>
2025-03-07 19:38:32 -0300

    feat: add input

---
+++ src/components/Input.tsx
@@ -0,0 +1,23 @@
+type Props = React.InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  isError?: boolean;
+  errorMessage?: string;
+};
+
+export function Input({ label, isError, errorMessage, id, ...rest }: Props) {
+  return (
+    <div>
+      <label htmlFor={id} className="text-[30px]">
+        {label}
+      </label>
+      <input
+        id={id}
+        className="bg-white text-[40px] text-black px-3 py-2 mt-1 rounded-md"
+        {...rest}
+      />
+      {isError && errorMessage && (
+        <p className="mt-3 text-[20px] text-red-400">{errorMessage}</p>
+      )}
+    </div>
+  );
+}
--- src/pages/CreatePage.tsx
+++ src/pages/CreatePage.tsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+import { Input } from "../components/Input";
 import { Wrapper } from "../components/Wrapper";
+import { ButtonLink } from "../components/ButtonLink";
+import { ROUTES } from "../constants/routes";
+import { Button } from "../components/Button";
 
 export function CreatePage() {
-  return <Wrapper>Create page</Wrapper>;
+  const [queueName, setQueueName] = useState("");
+  const [queueNameError, setQueueNameError] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQueueName(event.target.value);
+    setQueueNameError(false);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (queueName === "") {
+      setQueueNameError(true);
+      return;
+    }
+
+    alert("queueName = " + queueName);
+  };
+
+  return (
+    <Wrapper>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-16">
+        <Input
+          id="queue-name"
+          label="Nome da Fila"
+          isError={queueNameError}
+          errorMessage="Insira um nome para a fila por favor"
+          value={queueName}
+          onChange={handleChange}
+        />
+        <div className="flex flex-col gap-8">
+          <Button type="submit">Continuar</Button>
+          <ButtonLink to={ROUTES.HOME}>Voltar</ButtonLink>
+        </div>
+      </form>
+    </Wrapper>
+  );
 }
