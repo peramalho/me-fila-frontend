e8fda38940ccde60021ade67139c9d7ea765495c
peramalho <pedrorfig27@gmail.com>
2025-03-08 14:59:25 -0300

    feat: Add join page

--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import { Input } from "../components/Input";
 import { Wrapper } from "../components/Wrapper";
+import { ButtonLink } from "../components/ButtonLink";
+import { ROUTES } from "../constants/routes";
+import { Button } from "../components/Button";
+import { ButtonGroup } from "../components/ButtonGroup";
 
 export function JoinPage() {
-  return <Wrapper>Join Page</Wrapper>;
+  const [queueId, setQueueId] = useState("");
+  const [queueIdError, setQueueIdError] = useState(false);
+  const [username, setUsername] = useState("");
+
+  const handleChangeQueueId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQueueId(event.target.value);
+    setQueueIdError(false);
+  };
+
+  const handleChangeUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (queueId === "") {
+      setQueueIdError(true);
+      return;
+    }
+
+    alert("queueId = " + queueId + "" + " | username = " + username);
+  };
+
+  return (
+    <Wrapper>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-16 w-full">
+        <div className="w-full flex flex-col gap-4">
+          <Input
+            id="queue-id"
+            label="ID da Fila"
+            isError={queueIdError}
+            errorMessage="Insira um id por favor"
+            value={queueId}
+            onChange={handleChangeQueueId}
+          />
+          <Input
+            id="username"
+            label="Seu Nome"
+            value={username}
+            onChange={handleChangeUsername}
+          />
+        </div>
+        <ButtonGroup>
+          <Button type="submit">Continuar</Button>
+          <ButtonLink to={ROUTES.HOME}>Voltar</ButtonLink>
+        </ButtonGroup>
+      </form>
+    </Wrapper>
+  );
 }
