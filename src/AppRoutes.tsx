cc4dc5b45f72e129d38872f9ea4f31a7ecbf05af
peramalho <pedrorfig27@gmail.com>
2025-03-21 08:55:37 -0300

    feat: add fallback route redirecting to HOME

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { ROUTES } from "./constants/routes";
+import { HomePage } from "./pages/home";
+import { HostPage } from "./pages/host";
+import { JoinPage } from "./pages/join";
+import { HostIdPage } from "./pages/host/[id]";
+
+export function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<HomePage />} />
+        <Route path={ROUTES.HOST}>
+          <Route index element={<HostPage />} />
+          <Route path=":id" element={<HostIdPage />} />
+        </Route>
+        <Route path={ROUTES.JOIN}>
+          <Route index element={<JoinPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to={ROUTES.HOME} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
