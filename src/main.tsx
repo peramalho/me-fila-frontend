9f8abd1a04c450bf1610f337fce288838507c71a
peramalho <pedrorfig27@gmail.com>
2025-03-21 09:12:10 -0300

    feat: add auth functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
-import "./index.css";
-import { HomePage } from "./pages/home";
-import { HostPage } from "./pages/host";
-import { ROUTES } from "./constants/routes";
-import { JoinPage } from "./pages/join";
-import { HostIdPage } from "./pages/host/[id]";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./providers/AuthProvider";
+import { AppRoutes } from "./AppRoutes";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path={ROUTES.HOME} element={<HomePage />} />
-          <Route path={ROUTES.HOST}>
-            <Route index element={<HostPage />} />
-            <Route path=":id" element={<HostIdPage />} />
-          </Route>
-          <Route path={ROUTES.JOIN}>
-            <Route index element={<JoinPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
 );
---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,32 @@
+import { useCallback, useState } from "react";
+import { AuthContext } from "./contexts";
+import { HOST_TOKEN } from "../constants/localStorage";
+
+export type AuthContextType = {
+  hostToken: string | null;
+  login: (hostToken: string) => void;
+  logout: () => void;
+} | null;
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [hostToken, setHostToken] = useState<string | null>(() => {
+    const storedHostToken: string | null = localStorage.getItem(HOST_TOKEN);
+    return storedHostToken;
+  });
+
+  const login = useCallback((currentHostToken: string) => {
+    localStorage.setItem(HOST_TOKEN, currentHostToken);
+    setHostToken(currentHostToken);
+  }, []);
+
+  const logout = useCallback(() => {
+    localStorage.removeItem(HOST_TOKEN);
+    setHostToken(null);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ hostToken, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
