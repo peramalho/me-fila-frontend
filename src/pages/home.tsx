da08211762da58e9f858e68b8e9e690636dfd335
peramalho <pedrorfig27@gmail.com>
2025-03-30 11:55:54 -0300

    feat: redirect to /host from / if hostToken exists

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,17 @@
+import { Navigate } from "react-router";
 import { ButtonGroup } from "../components/ButtonGroup";
 import { ButtonLink } from "../components/ButtonLink";
 import { Wrapper } from "../components/Wrapper";
 import { ROUTES } from "../constants/routes";
+import { useAuth } from "../providers/useAuth";
 
 export function HomePage() {
+  const { hostToken } = useAuth();
+
+  if (hostToken) {
+    return <Navigate to={ROUTES.HOST} />;
+  }
+
   return (
     <Wrapper>
       <h1 className="text-6xl">Me Fila</h1>
